23066f71bd1d6e2223c19e7b93a95d9909b2c75f
pritam guha <pritam@tenxerlabs.com>
2024-02-25 23:57:51 +0530

    changed the navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,29 +1,22 @@
-import React, {useState} from 'react';
+import React from "react";
 
-const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
 
-    const handleMenuClick = () => {
-        setMenuOpen(!menuOpen);
-    };
-    
+export const Navbar = () => {
     return (
-        <nav className='flex justify-between items-center bg-gray-900 text-white sticky top-0 font-semibold py-2 px-6 font-sans'>
-            <div className='text-[1.8rem] m-1'>Pritam Guha</div>
-            <div className="absolute top-[1.5rem] right-[0.5rem] flex flex-col justify-between w-9 h-8" onClick={handleMenuClick}>
-                <span className="bg-white h-1 w-full rounded-md"></span>
-                <span className="bg-rose-500 h-1 w-full rounded-md"></span>
-                <span className="bg-white h-1 w-full rounded-md"></span>
+        <nav className="flex justify-between items-center bg-gray-900 text-white sticky top-0 w-full px-2.3rem py-0.5rem font-bold font-sans">
+            <div className="text-white font-normal text-xl m-4 no-underline">
+                Pritam Guha
             </div>
-            <div>
-                <ul className='flex space-x-10 text-[1.2rem]'>
-                    <li className='block text-white rounded-lg hover:bg-red-400 p-2'>Home</li>
-                    <li className='block text-white rounded-lg hover:bg-red-400 p-2'>Project</li>
-                    <li className='block text-white rounded-lg hover:bg-red-400 p-2'>About</li>
-                </ul>
+            <div className="menu">
+                <span className="bg-white"></span>
+                <span className="bg-rose-500"></span>
+                <span className="bg-white"></span>
             </div>
+            <ul className="flex p-2">
+                <li className="text-white p-4 mx-1 rounded-lg hover:bg-red-500">Blog</li>
+                <li className="text-white p-4 mx-1 rounded-lg hover:bg-red-500">About</li>
+                <li className="text-white p-4 mx-1 rounded-lg hover:bg-red-500">Home</li>
+            </ul>
         </nav>
-    )
+    );
 }
-
-export default Navbar
