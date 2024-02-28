d88a5481c6e5e5340811ddec04040942b226d9ec
pritam guha <pritam@tenxerlabs.com>
2024-02-28 11:51:30 +0530

    fixed the functionality and change some css

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -20,25 +20,29 @@ const ReviewForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('Form submitted:', { firstName, lastName, review });
+
+        setFirstName('');
+        setLastName('');
+        setReview('');
     };
 
 return (
     <div className="max-w-md mx-auto">
         <h2 className="text-2xl font-semibold mb-4">Leave a Review</h2>
-        <form className="space-y-4" onSubmit={handleSubmit}>
+        <form className="space-y-4 border-[0.8rem] border-gray-500 rounded-md p-3" onSubmit={handleSubmit}>
             <div>
                 <label className="block mb-1 text-[1.3rem] font-[500]">First Name:</label>
-                <input onChange={handleFirstNameChange} value={firstName} id="firstName" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-500" required />
+                <input onChange={handleFirstNameChange} value={firstName} id="firstName" className="w-full px-4 py-2 border-[0.2rem] border-gray-500 rounded-md focus:outline-none focus:ring focus:ring-blue-500" required />
             </div>
 
             <div>
                 <label className="block mb-1 text-[1.3rem] font-[500]">Last Name:</label>
-                <input onChange={handleLastNameChange} value={lastName} id="lastName" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-500" required />
+                <input onChange={handleLastNameChange} value={lastName} id="lastName" className="w-full px-4 py-2 border-[0.2rem] border-gray-500 rounded-md focus:outline-none focus:ring focus:ring-blue-500" required />
             </div>
 
             <div>
                 <label className="block mb-1 text-[1.3rem] font-[500]">Review:</label>
-                <textarea onChange={handleReviewChange} value={review} id="review" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-500" required />
+                <textarea onChange={handleReviewChange} value={review} id="review" className="w-full px-4 py-2 border-[0.2rem] border-gray-500 rounded-md focus:outline-none focus:ring focus:ring-blue-500" required />
             </div>
 
             <div>
